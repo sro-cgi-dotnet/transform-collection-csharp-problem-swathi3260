10c3c9f597eb72dd24ba82f2a9424e982297fb85
swathi3260 <swathi.peesapati@cgi.com>
2018-09-12 15:38:02 +0530

    add map

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 
 namespace Transform
 {
+    public delegate int delegateFunction(int x);
     public static class Transform 
     {
-        public static void Map()
-        {
-            throw new NotImplementedException();
+        public static int[] Map(this int[] a1,delegateFunction equation){
+              for(int i=0;i<a1.Length;i++)  {
+                  a1[i]=equation(a1[i]);
+              }
+              return a1;
         }
-    }
+
+}
 }
